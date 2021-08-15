d58baaaf50ec38515fa57b7de40ba6cb3fa2f337
vishal00923 <vishalbg.mz@gmail.com>
2021-08-15 23:10:09 +0530

    :sparkles: Add sticky menu background

--- js/main.js
+++ js/main.js
@@ -12,6 +12,15 @@ function initMap() {
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
+/* Sticky Menu Background */
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
 /* Smooth Scrolling */
 $('#navbar a, .btn').on('click', function (event) {
     if (this.hash !== '') {
