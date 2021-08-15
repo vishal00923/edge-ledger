3e2df740c006fae2bc73d86566bffcdd8198ecfc
vishal00923 <vishalbg.mz@gmail.com>
2021-08-15 23:07:39 +0530

    :sparkles: Add smooth scrolling effect

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ function initMap() {
     /* The marker, positioned at location */
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+/* Smooth Scrolling */
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100,
+            },
+            600
+        );
+    }
+});
